aa90d42a666cb306f1ef8a5b0a93db95577ef634
Indra Arga Muria <indraargaaa@gmail.com>
2025-04-08 00:47:55 +0700

    Modify Reusable

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,8 +7,8 @@ type CardProps = {
   export default function Card({ title, description }: CardProps) {
     return (
       <div className="rounded-xl shadow-md p-4 bg-white">
-        <h2 className="text-xl font-bold text-gray-800">{title}</h2>
-        <p className="text-gray-600 dark:text-gray-300">{description}</p>
+        <h2 className="text-xl font-bold text-teal-800">{title}</h2>
+        <p className="text-gray-800 text-sm">{description}</p>
       </div>
     );
   }
