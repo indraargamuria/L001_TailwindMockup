a02fccd9c3060a3d69404d06d5be0817062b6d24
Indra Arga Muria <indraargaaa@gmail.com>
2025-04-09 23:36:43 +0700

    Remove unnecessary class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ import { cards } from '@/data/dummydata';
 export default function Home() {
   return (
     <div>
-      <div className="flex grid grid-cols-6 mt-2">
+      <div className="grid grid-cols-6 mt-2">
         <div></div>
-        <div className="flex grid grid-cols-3 gap-2 col-span-4 col-start-2">
+        <div className="grid grid-cols-3 gap-2 col-span-4 col-start-2">
           {/* <div className="card">
             <img src="https://picsum.photos/1000" alt="" />
             <span>a</span>
