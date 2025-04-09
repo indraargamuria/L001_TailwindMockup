ec5ccf1cf84b20a437ae1d77f9b1c3cc082edf6e
Indra Arga Muria <indraargaaa@gmail.com>
2025-04-09 23:36:13 +0700

    Add hover

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,7 +10,7 @@ type CardProps = {
         <img src="https://picsum.photos/600" className="h-60 w-full object-bottom object-cover" alt="" />
         <h2 className="text-xl font-bold text-teal-800">{title}</h2>
         <p className="text-gray-800 text-sm">{description}</p>
-        <button className="bg-gray-700 w-full rounded text-white p-2 shadow">Add</button>
+        <button className="bg-gray-700 w-full rounded text-white p-2 shadow hover:bg-orange-600 transition-colors">Add</button>
       </div>
     );
   }
